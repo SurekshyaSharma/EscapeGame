ba15d5f6dd6e38027e1bccc834c4bb527b1bc81f
Surekshya Sharma <xst-shar3415@stthomas.edu>
2020-05-05 11:38:26 -0500

    button color changed not using bootstrap

--- main.js
+++ main.js
@@ -23,3 +23,30 @@ function seeMore() {
       moreText.style.display = "inline";
     }
   }
+  // form submission--------------------------------------------------------------------------------------------------------
+  function registerUser() {
+    var Username = document.getElementById("txtusername").value;
+    var Firstname = document.getElementById("txtfirstname").value;
+    var Lastname = document.getElementById("txtlastname").value;
+    var Email = document.getElementById("txtemail").value;
+    var Password = document.getElementById("txtpassword").value;
+    var Confirmpass = document.getElementById("passwordconfirm").value;
+
+      db = window.openDatabase("SoccerEarth", "2.0", "SoccerEarthDB", 2*1024*1024);
+      db.transaction(function(tx) {
+       NewUser(tx, Username, Firstname, Lastname, Email, Password, Confirmpass);
+       }, errorRegistration, successRegistration);
+       }
+
+       function NewUser(tx, Username, Firstname, Lastname, Email, Password, Confirmpass) {
+       var _Query = ("INSERT INTO SoccerEarth(UserName, FirstName, LastName, Email, Password, CPass) values ('"+ Username +"','"+ Firstname +"','"+ Lastname +"','"+ Email +"', '"+ Password +"', '"+ Confirmpass +"')");
+        alert(_Query);
+        tx.executeSql(_Query);
+        }
+        function errorRegistration(error) {
+     navigator.notification.alert(error, null, "Got an error mate", "cool");
+         }
+        function successRegistration() {
+        navigator.notification.alert("User data has been registered", null, "Information", "ok");
+          $( ":mobile-pagecontainer" ).pagecontainer( "change", "#page4" );
+      }
